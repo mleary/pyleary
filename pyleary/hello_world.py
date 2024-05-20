3c363a0d8ada1b0bc73e6b85437bd6d3d209f6dd
Matt Leary <33221468+mleary@users.noreply.github.com>
2024-05-19 22:53:05 -0400

    adding basic structure and initial test

---
+++ pyleary/hello_world.py
@@ -0,0 +1,4 @@
+
+
+def hello_world():
+    print("Creating a basic shell first.")
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyleary',
+    version='0.1.0',
+    packages=find_packages(),
+    description='A simple Python package for my personal use',
+    author='Matt Leary',
+)
