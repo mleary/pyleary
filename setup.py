db7cc750ab165200a606a87d1af5d8bae2cb977c
Matt Leary <33221468+mleary@users.noreply.github.com>
2024-05-27 15:49:13 -0400

    update unittest, setup

--- setup.py
+++ setup.py
@@ -6,4 +6,6 @@ setup(
     packages=find_packages(),
     description='A simple Python package for my personal use',
     author='Matt Leary',
+    url='https://github.com/mleary/pyleary',
+    install_requires=['json', 'markdown', 'textwrap'],
 )
